b14fc3e6f1b7fecc80f926d560384b7ac2e2618b
jo0oy <rkfkrh07@naver.com>
2024-04-04 00:56:05 +0900

    [init] project init

---
+++ src/main/java/com/jo0oy/cafeorder/CafeOrderApplication.java
@@ -0,0 +1,13 @@
+package com.jo0oy.cafeorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jo0oy/cafeorder/CafeOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jo0oy.cafeorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
