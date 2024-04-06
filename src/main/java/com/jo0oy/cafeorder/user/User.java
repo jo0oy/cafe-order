f1a054d6a207208a01d918804040542a0b7581c4
jo0oy <rkfkrh07@naver.com>
2024-04-06 21:10:16 +0900

    [feat] User(회원) 엔티티 구현
    related to #4

---
+++ src/main/java/com/jo0oy/cafeorder/user/User.java
@@ -0,0 +1,40 @@
+package com.jo0oy.cafeorder.user;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user")
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String username;
+
+    @Column(nullable = false, length = 150)
+    private String password;
+
+    @Column(nullable = false, length = 30)
+    private String phoneNumber;
+
+    @Builder
+    private User(
+        Long id,
+        String username,
+        String password,
+        String phoneNumber
+    ) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+}
